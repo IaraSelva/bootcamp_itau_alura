31a9e6e6a89d0db0b9794168722a4bbcafa6032f
iara <iaiaselva.is@gmail.com>
2021-07-06 12:01:17 -0300

    geração e validação de token + swagger

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,28 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
@@ -39,9 +44,10 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	@Cacheable(value = "listaTopicos")
-	public Page<TopicoDto> lista(@RequestParam (required = false) String nomeCurso, @PageableDefault(sort = "id", size = 3, direction = Sort.Direction.ASC) Pageable paginacao) {
-
+	@Cacheable(value = "listaDeTopicos")
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
+			@PageableDefault(sort = "dataCriacao", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
+		
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(topicos);
@@ -53,8 +59,8 @@ public class TopicosController {
 	
 	@PostMapping
 	@Transactional
-	@CacheEvict(value = "listaTopicos", allEntries = true)
-	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Validated TopicoForm form, UriComponentsBuilder uriBuilder) {
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
 		
@@ -74,8 +80,8 @@ public class TopicosController {
 	
 	@PutMapping("/{id}")
 	@Transactional
-	@CacheEvict(value = "listaTopicos", allEntries = true)
-	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Validated AtualizacaoTopicoForm form) {
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
 			Topico topico = form.atualizar(id, topicoRepository);
@@ -87,7 +93,7 @@ public class TopicosController {
 	
 	@DeleteMapping("/{id}")
 	@Transactional
-	@CacheEvict(value = "listaTopicos", allEntries = true)
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> remover(@PathVariable Long id) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
@@ -99,10 +105,3 @@ public class TopicosController {
 	}
 
 }
-
-
-
-
-
-
-
--- forum/src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
+++ forum/src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -1,28 +1,23 @@
 package br.com.alura.forum.controller.form;
 
-import com.sun.istack.NotNull;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
 
 public class AtualizacaoTopicoForm {
 	
-	@NotNull
+	@NotNull @NotEmpty @Length(min = 5)
 	private String titulo;
 	
-	@NotNull
+	@NotNull @NotEmpty @Length(min = 10)
 	private String mensagem;
-
-	public String getTitulo() {
-		return titulo;
-	}
 
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
-	}
-
-	public String getMensagem() {
-		return mensagem;
 	}
 
 	public void setMensagem(String mensagem) {
